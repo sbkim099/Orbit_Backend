e8ebd7113ffe11c8aac7b7133c0977f6af1abe30
김수빈 <sbkim099@naver.com>
2026-06-08 10:19:30 +0900

    회의실 예약 기능 구현 중

--- src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsController.java
+++ src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsController.java
@@ -1,7 +1,23 @@
 package com.study.app.domains.meetingRooms;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/meetingRooms")
 public class MeetingRoomsController {
+	
+	@Autowired
+	private MeetingRoomsService roomServ;
+	
+	@GetMapping("getAllRooms")
+	public ResponseEntity<List<MeetingRoomsDTO>> getAllRooms(){
+		List<MeetingRoomsDTO> list = roomServ.getAllRooms();
+		return ResponseEntity.ok(list);
+	}
 }
